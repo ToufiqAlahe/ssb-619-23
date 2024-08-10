2bc5f7d126284977fe83c6d0aa751c943ca19e4a
ToufiqAlahe <164491805+ToufiqAlahe@users.noreply.github.com>
2024-08-10 12:45:55 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,47 @@
+/* ex-1*/ 
+function showText(){
+	document.getElementById('s_text').innerHTML="Hello SSB";
+}
+// ex2
+function showText2(){
+	document.getElementById('s_text2').innerHTML="SSB means shikhbey sobai";
+}
+
+/* ex3 */
+function showText3(){
+	document.getElementById('s_text3').innerHTML="Hello ssb new batch";
+}
+// ex4
+function showText4(){
+	document.getElementById('s_text4').innerHTML="hello old ssb batch";
+}
+
+/* ex5 */
+function showText5(){
+	document.getElementById('s_text5').innerHTML="Hello ssb-619";
+}
+
+/* ex6 */
+function showText6(){
+	document.getElementById('s_text6').innerHTML="web development";
+}
+
+/* ex7 */
+function showText7(){
+	document.getElementById('s_text7').innerHTML="Do you know about it?";
+}
+
+/* ex8 */
+function showText8(){
+	document.getElementById('s_text8').innerHTML="javscript is oop language";
+}
+
+/* ex9 */
+function showText9(){
+	document.getElementById('s_text9').innerHTML="it is very easy to learn but time consuming";
+}
+
+/* ex10 */
+function showText10(){
+	document.getElementById('s_text10').innerHTML="Successfull";
+}
